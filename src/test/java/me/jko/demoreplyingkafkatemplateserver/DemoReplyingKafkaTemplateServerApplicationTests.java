884e89cbe1de53f9d5117b1df31f769005646fc0
Junhee Ko <jko@rainist.com>
2021-02-10 00:21:40 +0900

    first commit

---
+++ src/test/java/me/jko/demoreplyingkafkatemplateserver/DemoReplyingKafkaTemplateServerApplicationTests.java
@@ -0,0 +1,14 @@
+package me.jko.demoreplyingkafkatemplateserver;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReplyingKafkaTemplateServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
